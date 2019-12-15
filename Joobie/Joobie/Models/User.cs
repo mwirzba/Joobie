e1b4cfea3d790c547e886677fde3d73631083b11
Paweł Zimny <pawel.zimny@hotmail.com>
2019-12-15 21:00:33 +0100

    Added IAuthRepository. User model updated

--- Joobie/Joobie/Models/User.cs
+++ Joobie/Joobie/Models/User.cs
@@ -3,13 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Joobie.Models
 {
     public class User : IdentityUser
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
         public string SurName { get; set; }
+        [Required]
         public string YOB { get; set; }
     }
 }
