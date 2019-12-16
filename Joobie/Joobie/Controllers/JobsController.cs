ba9a9a747e2698d2e57bbd7d05c8f769cb558732
Matetus <mateuszwir23@gmail.com>
2019-12-16 15:03:46 +0100

    Added filtering properties,changed SearchForm

--- Joobie/Joobie/Controllers/JobsController.cs
+++ Joobie/Joobie/Controllers/JobsController.cs
@@ -15,13 +15,15 @@ namespace Joobie.Controllers
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<IActionResult> ListAsync(string searchString,string citySearchString,int[] categories)
+        public async Task<IActionResult> ListAsync(string searchString,string citySearchString,int[] categories,int[] typesOfContracts, int[] workingHours)
         {
             var listOfProperties = await _unitOfWork.Jobs.GetListsOfPropertiesAsync();
             ViewData["Categories"] = listOfProperties.Categories;
+            ViewData["TypesOfContracts"] = listOfProperties.TypesOfContract;
+            ViewData["WorkingHours"] = listOfProperties.WorkingHours; 
             ViewData["CurrentFilter"] = searchString;
             ViewData["CurrentCityFilter"] = citySearchString;
-            IEnumerable<Job> jobs = await GetSortedAndFilteredJobListAsync(searchString, citySearchString,new List<int>(categories));
+            IEnumerable<Job> jobs = await GetSortedAndFilteredJobListAsync(searchString, citySearchString,new List<int>(categories), new List<int>(typesOfContracts), new List<int>(workingHours));
             return View("List", jobs);
         }
 
@@ -119,7 +121,7 @@ namespace Joobie.Controllers
             job.CompanyId = findJobResult.Id; 
         }
 
-        private async Task<IEnumerable<Job>> GetSortedAndFilteredJobListAsync(string jobNameSearchString, string citySearchString,List<int> categories)
+        private async Task<IEnumerable<Job>> GetSortedAndFilteredJobListAsync(string jobNameSearchString, string citySearchString,List<int> categories,List<int> typesOfContracts, List<int> workingHours)
         {
             var predicate = PredicateBuilder.New<Job>();
             predicate.DefaultExpression = j => true; 
@@ -134,6 +136,14 @@ namespace Joobie.Controllers
             if (categories.Count>0)
             {
                  predicate = predicate.And(j => categories.Contains(j.CategoryId));
+            }
+            if (typesOfContracts.Count > 0)
+            {
+                predicate = predicate.And(j => typesOfContracts.Contains(j.TypeOfContractId));
+            }
+            if (workingHours.Count > 0)
+            {
+                predicate = predicate.And(j => workingHours.Contains(j.WorkingHoursId));
             }
             var jobs = await _unitOfWork.Jobs.GetJobsWithAllPropertiesByFilterAsync(predicate);
             return jobs;
