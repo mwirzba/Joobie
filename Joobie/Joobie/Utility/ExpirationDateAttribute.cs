90a66c99e799e870f93084e530fc55de7f4a5a1f
ketoprom <32978578+ketoprom@users.noreply.github.com>
2020-05-14 20:40:20 +0200

    Updated Validator

--- Joobie/Joobie/Utility/ExpirationDateAttribute.cs
+++ Joobie/Joobie/Utility/ExpirationDateAttribute.cs
@@ -20,15 +20,22 @@ namespace Joobie.Utility
             if (jobItem.ExpirationDate is null)
             {
                 return new ValidationResult(GetErrorMessage1());
+            }else if (jobItem.AddedDate is null)
+            {
+                var dateExpLocal = jobItem.ExpirationDate;
+                if (DateTime.Compare((DateTime)dateExpLocal, DateTime.Now) <0)
+                {
+                    return new ValidationResult(GetErrorMessage3());
+                }
+                return ValidationResult.Success;
             }
             var dateExp = jobItem.ExpirationDate;
             var dateAdd = jobItem.AddedDate;
             int res = DateTime.Compare((DateTime)jobItem.ExpirationDate, (DateTime)jobItem.AddedDate);
-            if(res <= 0)
+            if (res <= 0)
             {
                 return new ValidationResult(GetErrorMessage2());
             }
-
             return ValidationResult.Success;
         }
         private string GetErrorMessage1()
@@ -38,6 +45,10 @@ namespace Joobie.Utility
         private string GetErrorMessage2()
         {
             return $"Data wygaśnięcia musi być późniejsza, niż data dodania";
+        }
+        private string GetErrorMessage3()
+        {
+            return $"Data wygaśnięcia musi być późniejsza, niż data dzisiejsza";
         }
     }
 }
